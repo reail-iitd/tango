3f60ef4044d576adc770b9f6f7692a2ee63d7866
shreshthtuli <34661516+shreshthtuli@users.noreply.github.com>
2019-09-23 16:55:27 +0530

    bug fixes

--- PyBullet/main.py
+++ PyBullet/main.py
@@ -8,6 +8,7 @@ def executeAction(inp):
 while True:
     # take input from user
     inp = args.input
+    # executeAction(inp)
     process = Thread(target=executeAction, args=[inp])
     process.start()
     # while images in folder, update images
