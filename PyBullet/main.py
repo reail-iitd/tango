b2d25efdad7a0777ca084ea080b7d8346dba84e0
shreshthtuli <shreshthtuli@gmail.com>
2019-10-20 17:53:19 +0530

    updated main.py

--- PyBullet/main.py
+++ PyBullet/main.py
@@ -5,7 +5,6 @@ from threading import Thread
 def executeAction(inp):
     execute(convertActionsFromFile(inp))
 
-while True:
-    # take input from user
-    inp = args.input
-    executeAction(inp)
+# take input from user
+inp = args.input
+executeAction(inp)
