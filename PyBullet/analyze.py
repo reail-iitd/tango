4759c873a73fe73d15d45686b55658749649a86d
shreshthtuli <34661516+shreshthtuli@users.noreply.github.com>
2020-01-17 20:17:59 +0800

    Completed bottles dumpster data

--- PyBullet/analyze.py
+++ PyBullet/analyze.py
@@ -30,7 +30,7 @@ def printDatapoint(filename):
 	print(datapoint.toString(subSymbolic=False, metrics=False))
 	f.close()
 
-filename = './dataset/home/goal6-bottles-dumpster/world_home0/'
+filename = './dataset/home/goal6-bottles-dumpster/world_home3/'
 for i in range(len(listdir(filename))):
 	printDatapoint(filename+str(i))
 # printDatapoint(filename)
