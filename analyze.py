00756ecc10fe7ff9eccfd26798b533f57afb36dd
shreshthtuli <shreshthtuli@gmail.com>
2020-03-26 23:39:22 +0530

    updated code structure

---
+++ analyze.py
@@ -0,0 +1,358 @@
+from src.datapoint import *
+import pickle
+from os import listdir, remove, rename
+import math
+from scipy.spatial import distance
+from statistics import mean 
+from shutil import copyfile
+import json
+from extract_vectors import load_all_vectors
+import numpy as np
+import matplotlib.pyplot as plt
+from statistics import pstdev
+import seaborn as sns
+# from src.generalization import *
+
+GOAL_LISTS = \
+{'home': ["goal1-milk-fridge.json", "goal2-fruits-cupboard.json", "goal3-clean-dirt.json", "goal4-stick-paper.json", "goal5-cubes-box.json", "goal6-bottles-dumpster.json", "goal7-weight-paper.json", "goal8-light-off.json"],
+'factory': ["goal1-crates-platform.json", "goal2-paper-wall.json", "goal3-board-wall.json", "goal4-generator-on.json", "goal5-assemble-parts.json", "goal6-tools-workbench.json", "goal7-clean-water.json", "goal8-clean-oil.json"]}
+
+def printNumDatapoints(w='factory'):
+	totalpoints = 0
+	for goal in GOAL_LISTS[w]:
+		print('Goal = ' + goal)
+		goalpoints = 0
+		for world in range(10):
+			directory = './dataset/' + w + '/' + goal.split('.')[0] + '/world_' + w + str(world) + '/'
+			try:
+				numpoints = len(listdir(directory))
+			except Exception as e:
+				numpoints = 0
+			goalpoints += numpoints
+			print(numpoints, end = ' ')
+		totalpoints += goalpoints
+		print('\nTotal goal ' + goal + ' points = ' + str(goalpoints))
+	print('Gross total points = ' + str(totalpoints))
+
+def printDatapoint(filename):
+	print(filename)
+	f = open(filename + '.datapoint', 'rb')
+	datapoint = pickle.load(f)
+	print (datapoint.toString(subSymbolic=False))
+	print(datapoint.getTools(returnNoTool=True), datapoint.totalTime()/10000)
+	f.close()
+
+def keepNewDatapoints(idx=1):
+	for goal in GOAL_LIST:
+		print('Goal = ' + goal)
+		for world in range(10):
+			directoryOld = './dataset1/home/' + goal.split('.')[0] + '/world_home' + str(world)
+			directory = './dataset' + str(idx) + '/home/' + goal.split('.')[0] + '/world_home' + str(world)
+			try: numpoints = len(listdir(directory))
+			except Exception as e: numpoints = 0
+			try: oldpoints = len(listdir(directoryOld))
+			except Exception as e: oldpoints = 0
+			for i in range(numpoints):
+				if i < oldpoints: remove(directory + '/' + str(i) + '.datapoint')
+				else: rename(directory + '/' + str(i) + '.datapoint', directory + '/' + str(i - oldpoints) + '.datapoint')
+
+def printAllDatapoints():
+	for goal in GOAL_LIST['factory']:
+		print('Goal = ' + goal)
+		for world in range(10):
+			directory = './dataset/factory/' + goal.split('.')[0] + '/world_factory' + str(world)
+			try:
+				points = listdir(directory)
+			except Exception as e:
+				continue
+			for point in points:
+				printDatapoint(directory + '/' + point.split('.')[0])
+
+def changeAllDatapoints():
+	for goal in GOAL_LIST:
+		for world in range(10):
+			directory = './dataset/home/' + goal.split('.')[0] + '/world_home' + str(world) + '/'
+			for point in range(len(listdir(directory))):
+				file = directory + str(point) + '.datapoint'
+				f = open(file, 'rb')
+				datapoint = pickle.load(f)
+				f.close()
+				if True:
+					f = open(file, 'wb')
+					datapoint.world = 'world_home' + str(world)
+					datapoint.goal = goal.split('.')[0]
+					pickle.dump(datapoint, f)
+					f.flush()
+					f.close()
+
+def getTiming(goal='goal1-milk-fridge.json'):
+	for world in range(10):
+		directory = './dataset/home/' + goal.split('.')[0] + '/world_home' + str(world) + '/'
+		times = []; actions = []; subactions = []
+		for point in range(len(listdir(directory))):
+			file = directory + str(point) + '.datapoint'
+			f = open(file, 'rb')
+			datapoint = pickle.load(f)
+			times.append(totalTime(datapoint))
+			actions.append(len(datapoint.symbolicActions))
+			subactions.append(len(datapoint.actions))
+		# print('World ' + str(world) + ' min = ' + str(min(times)) + ' avg = ' + str(mean(times)) + ' max = ' + str(max(times)))
+		# print('World ' + str(world) + ' min = ' + str(min(actions)) + ' avg = ' + str(mean(actions)) + ' max = ' + str(max(actions)))
+		print('World ' + str(world) + ' min = ' + str(min(subactions)) + ' avg = ' + str(mean(subactions)) + ' max = ' + str(max(subactions)))
+
+def getInteractedObjs(datapoint):
+	objs =  json.load(open("jsons/objects.json", "r"))["objects"]
+	objs = [i['name'] for i in objs]
+	os = []
+	for action in datapoint.symbolicActions:
+		if not (str(action[0]) == 'E' or str(action[0]) == 'U'):
+			try: 
+				for i in [1,2]: 
+					if action[0]['args'][i] not in os and action[0]['args'][i] in objs: os.append(action[0]['args'][i])
+			except: pass
+	return os
+
+def getAllData():
+	domain = 'factory'
+	for goal in GOAL_LISTS[domain]:
+		print(goal)
+		times = []; actions = []; subactions = []; objs = []; tools = []
+		for world in range(10):
+			directory = './dataset/' + domain + '/' + goal.split('.')[0] + '/world_' + domain + str(world) + '/'
+			for point in range(len(listdir(directory))):
+				file = directory + str(point) + '.datapoint'
+				f = open(file, 'rb')
+				datapoint = pickle.load(f)
+				times.append(datapoint.totalTime()/1000)
+				actions.append(len(datapoint.symbolicActions))
+				subactions.append(len(datapoint.actions))
+				objs.append(len(getInteractedObjs(datapoint)))
+				tools.append(len(datapoint.getTools()))
+		print('Time       = ' + "{:.2f}".format(mean(times)) + ' +- ' + "{:.2f}".format(pstdev(times)/1000))
+		print('Actions    = ' + "{:.2f}".format(mean(actions)) + ' +- ' + "{:.2f}".format(pstdev(actions)))
+		print('SubActions = ' + "{:.2f}".format(mean(subactions)) + ' +- ' + "{:.2f}".format(pstdev(subactions)))
+		print('Objects    = ' + "{:.2f}".format(mean(objs)) + ' +- ' + "{:.2f}".format(pstdev(objs)))
+		print('Tools      = ' + "{:.2f}".format(mean(tools)) + ' +- ' + "{:.2f}".format(pstdev(tools)))
+
+def combineDatasets(idx=1):
+	for goal in GOAL_LIST:
+		for world in range(10):
+			directoryOld = './dataset/home/' + goal.split('.')[0] + '/world_home' + str(world)
+			directory = './dataset'+ str(idx) + '/home/' + goal.split('.')[0] + '/world_home' + str(world)
+			try: numpoints = len(listdir(directory))
+			except Exception as e: numpoints = 0
+			try: oldpoints = len(listdir(directoryOld))
+			except Exception as e: oldpoints = 0
+			for i in range(numpoints):
+				copyfile(directory + '/' + str(i) + '.datapoint', directoryOld + '/' + str(i + oldpoints) + '.datapoint')
+
+def printGraph(filename):
+	print(filename)
+	f = open(filename + '.datapoint', 'rb')
+	datapoint = pickle.load(f); f.close()
+	f = open("dataset/test/home/test2/0.graph", "w+")
+	f.write(json.dumps(datapoint.getGraph(), indent=2))
+
+def allActionTypes():
+	actionTypes = []
+	for goal in GOAL_LISTS['home']:
+		for world in range(10):
+			directory = './dataset/home/' + goal.split('.')[0] + '/world_home' + str(world) + '/'
+			for point in range(len(listdir(directory))):
+				file = directory + str(point) + '.datapoint'
+				datapoint = pickle.load(open(file, 'rb'))
+				for subAction in datapoint.symbolicActions:
+					if len(subAction) == 1 and not [subAction[0]['name'], len(subAction[0]['args'])] in actionTypes:
+						actionTypes.append([subAction[0]['name'], len(subAction[0]['args'])])
+	print(actionTypes)
+
+def allTools():
+	tools = []
+	for goal in GOAL_LISTS['factory']:
+		for world in range(10):
+			directory = './dataset/factory/' + goal.split('.')[0] + '/world_factory' + str(world) + '/'
+			for point in range(len(listdir(directory))):
+				file = directory + str(point) + '.datapoint'
+				datapoint = pickle.load(open(file, 'rb'))
+				for tool in datapoint.getTools():
+					if tool not in tools:
+						tools.append(tool)
+	print(tools)
+
+def checkActionTypes():
+	actionTypes = []
+	for goal in GOAL_LISTS['home']:
+		for world in range(10):
+			directory = './dataset/home/' + goal.split('.')[0] + '/world_home' + str(world) + '/'
+			for point in range(len(listdir(directory))):
+				file = directory + str(point) + '.datapoint'
+				datapoint = pickle.load(open(file, 'rb'))
+				for action in datapoint.symbolicActions:
+					if len(action) == 1:
+						possible = getPossiblePredicates(action[0]['name'])
+						for i in range(len(action[0]['args'])):
+							if not action[0]['args'][i] in possible[i]:
+								print(file)
+								print(action[0]['name'], i, action[0]['args'][i])
+
+def testData():
+	# for i in range(1,9):	formTestData(i)
+	formTestDataFactory(4)
+	# for i in range(1,9): formTestDataFactory(i)
+
+def printAllTimes():
+	for goal in ["goal2-fruits-cupboard.json"]:
+		print('Goal = ' + goal)
+		for world in range(1):
+			directory = './dataset/home/' + goal.split('.')[0] + '/world_home' + str(world)
+			try:
+				points = listdir(directory)
+			except Exception as e:
+				continue
+			for point in points:
+				printDatapoint(directory + '/' + point.split('.')[0])
+
+def getInterestTools(domain, numTools):
+	toolusage = {}
+	TOOLS2 = ['stool', 'tray', 'tray2', 'big-tray', 'book', 'box', 'chair',\
+			'stick', 'glue', 'tape', 'mop', 'sponge', 'vacuum'] if domain == 'home' else ['lift', \
+			'stool', 'trolley', 'stick', 'ladder', 'glue', 'tape', 'drill', '3d_printer', \
+			'screwdriver', 'brick', 'hammer', 'blow_dryer', 'box', 'wood_cutter', 'welder', \
+			'spraypaint', 'toolbox', 'mop']
+	for tool in TOOLS2:
+		toolusage[tool] = 0
+	for goal in GOAL_LISTS[domain]:
+		print('Goal = ' + goal)
+		for world in range(10):
+			directory = './dataset/'+domain+'/' + goal.split('.')[0] + '/world_'+domain + str(world)
+			try: points = listdir(directory)
+			except Exception as e: continue
+			for point in points:
+				filename = directory + '/' + point.split('.')[0]
+				f = open(filename + '.datapoint', 'rb')
+				datapoint = pickle.load(f)
+				ts = datapoint.getTools(returnNoTool=False)
+				for t in ts: toolusage[t] += 1
+				f.close()
+	sortedtools = sorted(toolusage.items(), key=lambda kv: kv[1], reverse=True)
+	print(sortedtools)
+	interestTools = [a[0] for a in sortedtools][:numTools + (2)]
+	if domain == 'factory': 
+		interestTools.remove('spraypaint'); interestTools.remove('3d_printer');
+	else:
+		interestTools.remove('tray2'); interestTools.remove('sponge')
+	return interestTools
+
+def mapToolsGoals():
+	numTools = 10; domain = 'home'
+	interestTools = getInterestTools(domain, numTools)
+	usemap = np.zeros((len(GOAL_LISTS[domain]), numTools))
+	for goal in GOAL_LISTS[domain]:
+		for world in range(10):
+			directory = './dataset/'+domain+'/' + goal.split('.')[0] + '/world_'+domain + str(world)
+			try: points = listdir(directory)
+			except Exception as e: continue
+			for point in points:
+				filename = directory + '/' + point.split('.')[0]
+				f = open(filename + '.datapoint', 'rb')
+				datapoint = pickle.load(f)
+				ts = datapoint.getTools(returnNoTool=False)
+				for t in ts:
+					if t in interestTools: usemap[GOAL_LISTS[domain].index(goal)][interestTools.index(t)] += 1
+				f.close()
+	print(interestTools)
+	print(usemap)
+	f, ax = plt.subplots(figsize=(4, 3))
+	ax = sns.heatmap(usemap, cmap="Reds", yticklabels=list(range(1,9)), xticklabels=interestTools, linewidths=1)
+	ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha='right')
+	ax.set_ylabel(domain.capitalize() + ' Goal ID')
+	plt.tight_layout()
+	f.savefig('figures/'+domain+'_goal_tools.pdf')
+
+def mapToolsWorlds():
+	numTools = 10; domain = 'home'
+	interestTools = getInterestTools(domain, numTools)
+	usemap = np.zeros((10, numTools))
+	for goal in GOAL_LISTS[domain]:
+		for world in range(10):
+			directory = './dataset/'+domain+'/' + goal.split('.')[0] + '/world_'+domain + str(world)
+			try: points = listdir(directory)
+			except Exception as e: continue
+			for point in points:
+				filename = directory + '/' + point.split('.')[0]
+				f = open(filename + '.datapoint', 'rb')
+				datapoint = pickle.load(f)
+				ts = datapoint.getTools(returnNoTool=False)
+				for t in ts:
+					if t in interestTools: usemap[world][interestTools.index(t)] += 1
+				f.close()
+	print(interestTools)
+	print(usemap)
+	f, ax = plt.subplots(figsize=(4, 3))
+	ax = sns.heatmap(usemap, cmap="Reds", yticklabels=list(range(10)), xticklabels=interestTools, linewidths=1)
+	ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha='right')
+	ax.set_ylabel(domain.capitalize() + ' Scene ID')
+	plt.tight_layout()
+	f.savefig('figures/'+domain+'_world_tools.pdf')
+
+def getInteractedObjects(filename):
+	f = open(filename + '.datapoint', 'rb')
+	datapoint = pickle.load(f); f.close()
+	os = []
+	for action in datapoint.symbolicActions:
+		if not (str(action[0]) == 'E' or str(action[0]) == 'U'):
+			try: 
+				for i in [1,2]: 
+					if action[0]['args'][i] not in os: os.append(action[0]['args'][i])
+			except: pass
+	return os
+
+def mapObjects():
+	numObj = 5; domain = 'factory'
+	objInteracted = {}
+	for obj in json.load(open("jsons/objects.json", "r"))["objects"]:
+		objInteracted[obj["name"]] = 0
+	for goal in GOAL_LISTS[domain]:
+		print('Goal = ' + goal)
+		for world in range(10):
+			directory = './dataset/'+domain+'/' + goal.split('.')[0] + '/world_'+domain + str(world)
+			points = listdir(directory)
+			for point in points:
+				filename = directory + '/' + point.split('.')[0]
+				for o in getInteractedObjects(filename): 
+					try: objInteracted[o] += 1
+					except: pass
+	sortedObj = sorted(objInteracted.items(), key=lambda kv: kv[1], reverse=True)
+	print(sortedObj)
+	interestObj = [a[0] for a in sortedObj][:numObj]
+	usemap = np.zeros((len(GOAL_LISTS[domain]), numObj))
+	for goal in GOAL_LISTS[domain]:
+		for world in range(10):
+			directory = './dataset/'+domain+'/' + goal.split('.')[0] + '/world_'+domain + str(world)
+			points = listdir(directory)
+			for point in points:
+				filename = directory + '/' + point.split('.')[0]
+				for o in getInteractedObjects(filename): 
+					if o in interestObj: usemap[GOAL_LISTS[domain].index(goal)][interestObj.index(o)] += 1
+	print(interestObj)
+	print(usemap)
+	f, ax = plt.subplots(figsize=(4, 3))
+	ax = sns.heatmap(usemap, cmap="Reds", yticklabels=[a.split('.')[0] for a in GOAL_LISTS[domain]], xticklabels=interestObj, linewidths=1)
+	plt.tight_layout()
+	f.savefig('figures/'+domain+'_objects.pdf')
+
+# keepNewDatapoints(4)
+# printAllDatapoints()
+# printNumDatapoints(w='home')
+# changeAllDatapoints()
+# combineDatasets(4)
+# printGraph("dataset/factory/goal1-crates-platform/world_factory3/0")
+# checkActionTypes()
+# printGraph("dataset/home/goal1-milk-fridge/world_home4/0")
+# testData()
+# printAllTimes()
+# allTools()
+# mapToolsGoals()
+# mapToolsWorlds()
+# mapObjects()
+getAllData()
---
+++ src/GNN/models.py
@@ -0,0 +1,296 @@
+from src.GNN.oldmodels import *
+from src.utils import *
+
+def action2vec(action, num_objects, num_states):
+    actionArray = torch.zeros(len(possibleActions))
+    actionArray[possibleActions.index(action['name'])] = 1
+    predicate1 = torch.zeros(num_objects+1)
+    predicate2 = torch.zeros(num_objects+1)
+    predicate3 = torch.zeros(num_states+1)
+    if len(action['args']) < 1:
+        predicate1[-1] = 1
+    if len(action['args']) < 2:
+        predicate1[object2idx[action['args'][0]]] = 1
+        predicate2[-1] = 1
+    else:
+        if action['args'][1] in object2idx:
+            predicate2[object2idx[action['args'][1]]] = 1
+            predicate3[-1] = 1
+        else:
+            predicate2[-1] = 1
+            predicate2[possibleStates.index(action['args'][1])] = 1
+    return torch.cat((actionArray, predicate1, predicate2, predicate3), 0)
+
+def vec2action(vec, num_objects):
+    pass
+
+############################ DGL ############################
+
+class HeteroRGCNLayer(nn.Module):
+    # Source = https://docs.dgl.ai/en/0.4.x/tutorials/hetero/1_basics.html
+    def __init__(self, in_size, out_size, etypes, activation):
+        super(HeteroRGCNLayer, self).__init__()
+        self.weight = nn.ModuleDict({name : nn.Linear(in_size, out_size) for name in etypes})
+        self.activation = activation
+
+    def forward(self, G, features):
+        funcs = {}
+        for etype in G.etypes:
+            Wh = self.weight[etype](features)
+            G.nodes['object'].data['Wh_%s' % etype] = Wh
+            funcs[etype] = (fn.copy_u('Wh_%s' % etype, 'm'), fn.mean('m', 'h'))
+        G.multi_update_all(funcs, 'sum')
+        return self.activation(G.nodes['object'].data['h'])
+
+class GatedHeteroRGCNLayer(nn.Module):
+    # Source = https://docs.dgl.ai/_modules/dgl/nn/pytorch/conv/gatedgraphconv.html#GatedGraphConv
+    def __init__(self, in_size, out_size, etypes, activation):
+        super(GatedHeteroRGCNLayer, self).__init__()
+        self.weight = nn.ModuleDict({name : nn.Linear(in_size, out_size) for name in etypes})
+        self.reduce = nn.Linear(in_size, out_size)
+        self.activation = activation
+        self.gru = LayerNormGRUCell(out_size, out_size, bias=True)
+
+    def forward(self, G, features):
+        funcs = {}; feat = self.activation(self.reduce(features))
+        for _ in range(N_TIMESEPS):
+            for etype in G.etypes:
+                Wh = self.weight[etype](features)
+                G.nodes['object'].data['Wh_%s' % etype] = Wh
+                funcs[etype] = (fn.copy_u('Wh_%s' % etype, 'm'), fn.mean('m', 'h'))
+            G.multi_update_all(funcs, 'sum')
+            feat = self.gru(G.nodes['object'].data['h'], feat)
+        return self.activation(feat)
+
+######################################################################################
+
+class DGL_Simple_Likelihood(nn.Module):
+    def __init__(self,
+                 in_feats,
+                 n_objects,
+                 n_hidden,
+                 n_classes,
+                 n_layers,
+                 etypes,
+                 activation,
+                 dropout,
+                 embedding,
+                 weighted):
+        super(DGL_Simple_Likelihood, self).__init__()
+        self.n_classes = n_classes
+        self.etypes = etypes
+        self.name = "GGCN_Metric_Attn_L_NT_" + ('C_' if 'c' in embedding else '') + ('W_' if weighted else '') + str(n_hidden) + "_" + str(n_layers)
+        self.layers = nn.ModuleList()
+        self.layers.append(nn.Linear(in_feats + n_objects*4, n_hidden))
+        for i in range(n_layers - 1):
+            self.layers.append(nn.Linear(n_hidden, n_hidden))
+        self.attention = nn.Linear(n_hidden + n_hidden, n_hidden)
+        self.attention2 = nn.Linear(n_hidden, 1)
+        self.embed = nn.Linear(PRETRAINED_VECTOR_SIZE, n_hidden)
+        self.fc1 = nn.Linear(3 * n_hidden, n_hidden)
+        self.fc2 = nn.Linear(n_hidden, n_hidden)
+        self.fc3 = nn.Linear(n_hidden, n_hidden)
+        self.fc4 = nn.Linear(n_hidden, 1)
+        self.p1  = nn.Linear(2 * n_hidden, n_hidden)
+        self.p2  = nn.Linear(n_hidden, 1)
+        self.final = nn.Sigmoid()
+        self.activation = nn.PReLU()
+
+    def forward(self, g, goalVec, goalObjectsVec, tool_vec):
+        h = g.ndata['feat']
+        edgeMatrices = [g.adjacency_matrix(etype=t) for t in self.etypes]
+        edges = torch.cat(edgeMatrices, 1).to_dense()
+        h = torch.cat((h, edges), 1)
+        for i, layer in enumerate(self.layers):
+            h = self.activation(layer(h))
+        goalObjectsVec = self.activation(self.embed(goalObjectsVec))
+        attn_embedding = torch.cat([h, goalObjectsVec.repeat(h.size(0)).view(h.size(0), -1)], 1)
+        attn_embedding = self.activation(self.attention(attn_embedding))
+        attn_weights = F.softmax(self.activation(self.attention2(attn_embedding)), dim=0)
+        # print(attn_weights)
+        scene_embedding = torch.mm(attn_weights.t(), h)
+        goal_embed = self.activation(self.embed(goalVec))
+        scene_and_goal = torch.cat([scene_embedding, goal_embed.view(1,-1)], 1)
+        l = []
+        tool_embedding = self.activation(self.embed(tool_vec))
+        for i in range(NUMTOOLS-1):
+            final_to_decode = torch.cat([scene_and_goal, tool_embedding[i].view(1, -1)], 1)
+            h = self.activation(self.fc1(final_to_decode))
+            h = self.activation(self.fc2(h))
+            h = self.activation(self.fc3(h))
+            h = self.final(self.fc4(h))
+            l.append(h.flatten())
+        tools = torch.stack(l)
+        probNoTool = self.activation(self.p1(scene_and_goal))
+        probNoTool = torch.sigmoid(self.activation(self.p2(probNoTool))).flatten()
+        output = torch.cat(((1-probNoTool)*tools.flatten(), probNoTool), dim=0)
+        return output
+
+class GGCN(nn.Module):
+    def __init__(self,
+                 in_feats,
+                 n_objects,
+                 n_hidden,
+                 n_classes,
+                 n_layers,
+                 etypes,
+                 activation,
+                 dropout):
+        super(GGCN, self).__init__()
+        self.name = "GGCN_" + str(n_hidden) + "_" + str(n_layers)
+        self.layers = nn.ModuleList()
+        self.layers.append(GatedHeteroRGCNLayer(in_feats, n_hidden, etypes, activation=activation))
+        for i in range(n_layers - 1):
+            self.layers.append(GatedHeteroRGCNLayer(n_hidden, n_hidden, etypes, activation=activation))
+        self.embed = nn.Linear(PRETRAINED_VECTOR_SIZE, n_hidden)
+        self.fc1 = nn.Linear(n_hidden + n_hidden, n_hidden)
+        self.fc2 = nn.Linear(n_hidden, n_hidden)
+        self.fc3 = nn.Linear(n_hidden, n_classes)
+        self.final = nn.Sigmoid()
+        self.activation = nn.PReLU()
+
+    def forward(self, g, goalVec, goalObjectsVec, tool_vec):
+        h = g.ndata['feat']
+        for i, layer in enumerate(self.layers):
+            h = layer(g, h)
+        scene_embedding = torch.sum(h, dim=0).view(1,-1)
+        goal_embed = self.activation(self.embed(goalVec))
+        h = torch.cat([scene_embedding, goal_embed.view(1,-1)], 1)
+        h = self.activation(self.fc1(h))
+        h = self.activation(self.fc2(h))
+        h = self.final(self.fc3(h))
+        return h
+
+class GGCN_Metric(nn.Module):
+    def __init__(self,
+                 in_feats,
+                 n_objects,
+                 n_hidden,
+                 n_classes,
+                 n_layers,
+                 etypes,
+                 activation,
+                 dropout):
+        super(GGCN_Metric, self).__init__()
+        self.etypes = etypes
+        self.name = "GGCN_Metric_" + str(n_hidden) + "_" + str(n_layers)
+        self.layers = nn.ModuleList()
+        self.layers.append(nn.Linear(in_feats + n_objects*4, n_hidden))
+        for i in range(n_layers - 1):
+            self.layers.append(nn.Linear(n_hidden, n_hidden))
+        self.embed = nn.Linear(PRETRAINED_VECTOR_SIZE, n_hidden)
+        self.fc1 = nn.Linear(n_hidden + n_hidden, n_hidden)
+        self.fc2 = nn.Linear(n_hidden, n_hidden)
+        self.fc3 = nn.Linear(n_hidden, n_classes)
+        self.final = nn.Sigmoid()
+        self.activation = nn.PReLU()
+
+    def forward(self, g, goalVec, goalObjectsVec, tool_vec):
+        h = g.ndata['feat']
+        edgeMatrices = [g.adjacency_matrix(etype=t) for t in self.etypes]
+        edges = torch.cat(edgeMatrices, 1).to_dense()
+        h = torch.cat((h, edges), 1)
+        for i, layer in enumerate(self.layers):
+            h = self.activation(layer(h))
+        scene_embedding = torch.sum(h, dim=0).view(1,-1)
+        goal_embed = self.activation(self.embed(goalVec))
+        h = torch.cat([scene_embedding, goal_embed.view(1,-1)], 1)
+        h = self.activation(self.fc1(h))
+        h = self.activation(self.fc2(h))
+        h = self.final(self.fc3(h))
+        return h
+
+class GGCN_Metric_Attn(nn.Module):
+    def __init__(self,
+                 in_feats,
+                 n_objects,
+                 n_hidden,
+                 n_classes,
+                 n_layers,
+                 etypes,
+                 activation,
+                 dropout):
+        super(GGCN_Metric_Attn, self).__init__()
+        self.etypes = etypes
+        self.name = "GGCN_Metric_Attn_" + str(n_hidden) + "_" + str(n_layers)
+        self.layers = nn.ModuleList()
+        self.layers.append(nn.Linear(in_feats + n_objects*4, n_hidden))
+        for i in range(n_layers - 1):
+            self.layers.append(nn.Linear(n_hidden, n_hidden))
+        self.attention = nn.Linear(n_hidden + n_hidden, 1)
+        self.embed = nn.Linear(PRETRAINED_VECTOR_SIZE, n_hidden)
+        self.fc1 = nn.Linear(n_hidden + n_hidden, n_hidden)
+        self.fc2 = nn.Linear(n_hidden, n_hidden)
+        self.fc3 = nn.Linear(n_hidden, n_classes)
+        self.final = nn.Sigmoid()
+        self.activation = nn.PReLU()
+
+    def forward(self, g, goalVec, goalObjectsVec, tool_vec):
+        h = g.ndata['feat']
+        edgeMatrices = [g.adjacency_matrix(etype=t) for t in self.etypes]
+        edges = torch.cat(edgeMatrices, 1).to_dense()
+        h = torch.cat((h, edges), 1)
+        for i, layer in enumerate(self.layers):
+            h = self.activation(layer(h))
+        goalObjectsVec = self.activation(self.embed(goalObjectsVec))
+        attn_embedding = torch.cat([h, goalObjectsVec.repeat(h.size(0)).view(h.size(0), -1)], 1)
+        attn_weights = F.softmax(self.activation(self.attention(attn_embedding)), dim=0)
+        scene_embedding = torch.mm(attn_weights.t(), h)
+        goal_embed = self.activation(self.embed(goalVec))
+        scene_and_goal = torch.cat([scene_embedding, goal_embed.view(1,-1)], 1)
+        h = self.activation(self.fc1(scene_and_goal))
+        h = self.activation(self.fc2(h))
+        h = self.final(self.fc3(h))
+        return h
+
+class GGCN_Metric_Attn_L(nn.Module):
+    def __init__(self,
+                 in_feats,
+                 n_objects,
+                 n_hidden,
+                 n_classes,
+                 n_layers,
+                 etypes,
+                 activation,
+                 dropout):
+        super(GGCN_Metric_Attn_L, self).__init__()
+        self.etypes = etypes
+        self.name = "GGCN_Metric_Attn_L_" + str(n_hidden) + "_" + str(n_layers)
+        self.layers = nn.ModuleList()
+        self.layers.append(nn.Linear(in_feats + n_objects*4, n_hidden))
+        for i in range(n_layers - 1):
+            self.layers.append(nn.Linear(n_hidden, n_hidden))
+        self.attention = nn.Linear(n_hidden + n_hidden, 1)
+        self.embed = nn.Linear(PRETRAINED_VECTOR_SIZE, n_hidden)
+        self.fc1 = nn.Linear(3 * n_hidden, n_hidden)
+        self.fc2 = nn.Linear(n_hidden, n_hidden)
+        self.fc3 = nn.Linear(n_hidden, n_hidden)
+        self.fc4 = nn.Linear(n_hidden, 1)
+        self.final = nn.Sigmoid()
+        self.activation = nn.PReLU()
+
+    def forward(self, g, goalVec, goalObjectsVec, tool_vec):
+        h = g.ndata['feat']
+        edgeMatrices = [g.adjacency_matrix(etype=t) for t in self.etypes]
+        edges = torch.cat(edgeMatrices, 1).to_dense()
+        h = torch.cat((h, edges), 1)
+        for i, layer in enumerate(self.layers):
+            h = self.activation(layer(h))
+        goalObjectsVec = self.activation(self.embed(goalObjectsVec))
+        attn_embedding = torch.cat([h, goalObjectsVec.repeat(h.size(0)).view(h.size(0), -1)], 1)
+        attn_weights = F.softmax(self.activation(self.attention(attn_embedding)), dim=0)
+        scene_embedding = torch.mm(attn_weights.t(), h)
+        goal_embed = self.activation(self.embed(goalVec))
+        scene_and_goal = torch.cat([scene_embedding, goal_embed.view(1,-1)], 1)
+        l = []
+        tool_vec2 = torch.cat((tool_vec, torch.zeros((1, PRETRAINED_VECTOR_SIZE))))
+        tool_embedding = self.activation(self.embed(tool_vec2))
+        for i in range(NUMTOOLS):
+            final_to_decode = torch.cat([scene_and_goal, tool_embedding[i].view(1, -1)], 1)
+            h = self.activation(self.fc1(final_to_decode))
+            h = self.activation(self.fc2(h))
+            h = self.activation(self.fc3(h))
+            h = self.final(self.fc4(h))
+            l.append(h.flatten())
+        tools = torch.stack(l)
+        return tools
