4763d9edbb5743fa470466bc4ff7118b760542b8
shreshthtuli <shreshthtuli@gmail.com>
2020-03-27 18:05:05 +0530

    cleaned up code

--- husky_ur5.py
+++ husky_ur5.py
@@ -578,4 +578,18 @@ def getDatapoint():
 def destroy():
   p.disconnect()
                       
+def executeAction(inp):
+    start(args)
+    if execute(convertActionsFromFile(inp), args.goal):
+    	print("Goal Success!!!")
+    else:
+    	print("Goal Fail!!!")
 
+if __name__ == '__main__':
+	# take input from user
+	inp = args.input
+	executeAction(inp)
+
+	datapoint = getDatapoint()
+	print(datapoint.toString(metrics=False))
+	saveDatapoint('test')
