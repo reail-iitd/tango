f208726829eb0041487c0387623302f38df734e2
shreshthtuli <shreshthtuli@gmail.com>
2020-05-22 14:22:46 +0530

    Update train.py

--- train.py
+++ train.py
@@ -149,6 +149,7 @@ def test_policy(dset, graphs, model, modelEnc, num_objects = 0, verbose = False)
 				elif err != '': error += 1; break
 	den = correct + incorrect + error
 	print ("Correct, Incorrect, Error: ", (correct*100/den), (incorrect*100/den), (error*100/den))
+	return (correct*100/den), (incorrect*100/den), (error*100/den)
 
 def accuracy_score(dset, graphs, model, modelEnc, num_objects = 0, verbose = False):
 	total_correct = 0
@@ -526,9 +527,9 @@ if __name__ == '__main__':
 			loss = backprop(data, optimizer, train_set, model, data.num_objects, modelEnc, batch_size = 1)
 			print(loss)
 			t1, t2 = eval_accuracy(data, train_set, test_set, model, modelEnc, True)
-			accuracy_list.append((t2, t1, loss))
 			if 'action' in training:
-				test_policy(data, test_set, model, modelEnc, data.num_objects, False)
+				c, i, e = test_policy(data, test_set, model, modelEnc, data.num_objects, False)
+			accuracy_list.append((t2, t1, loss, c, i, e))
 			save_model(model, optimizer, num_epochs, accuracy_list)
 		print ("The maximum accuracy on test set is ", str(max(accuracy_list)), " at epoch ", accuracy_list.index(max(accuracy_list)))
 
