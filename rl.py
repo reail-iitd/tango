bc5377628c7fe97316663114bb533ea1e82beaad
shreshthtuli <shreshthtuli@gmail.com>
2020-05-23 14:42:39 +0530

    updated rl code

--- rl.py
+++ rl.py
@@ -12,6 +12,7 @@ import torch
 import torch.utils.data
 import torch.nn as nn
 from torch.autograd import Variable
+import torch.nn.functional as F
 
 import warnings
 warnings.simplefilter("ignore")
@@ -37,6 +38,7 @@ graph_seq_length = 4
 num_actions = len(possibleActions)
 memory_size = 3000
 with open('jsons/embeddings/'+embedding+'.vectors') as handle: e = json.load(handle)
+avg = lambda a : sum(a)/len(a)
 
 def get_all_possible_actions():
 	actions = []
@@ -78,6 +80,18 @@ def load_dataset():
 	pickle.dump(data, open(filename, "wb"))
 	return data
 
+def load_buffer():
+	filename = 'dataset/rl_buffer.pkl'
+	print(filename)
+	if path.exists(filename):
+		return pickle.load(open(filename, 'rb'))
+	return pd.DataFrame(columns=['goal_num', 'st', 'at', 'p', 'st+1', 'r'])
+
+def save_buffer(replay_buffer):
+	filename = 'dataset/rl_buffer.pkl'
+	print("Buffer Size =", replay_buffer.shape[0])
+	pickle.dump(replay_buffer, open(filename, "wb"))
+
 def world_split(data):
 	test_set = []
 	train_set = []
@@ -102,12 +116,13 @@ def split_data(data):
 def form_initial_dataset():
 	data = load_dataset()
 	filename = 'dataset/rl_dataset.pkl'
+	print(filename)
 	if path.exists(filename): 
 		with open(filename, 'rb') as f:
 			df, init_graphs, test_set = pickle.load(f)
 		return data, df, init_graphs, test_set
 	train_set, test_set = split_data(data)
-	df = pd.DataFrame(columns=['goal_num', 'st', 'at', 'st+1', 'r'])
+	df = pd.DataFrame(columns=['goal_num', 'st', 'at', 'p', 'stv', 'st+1', 'r'])
 	init_graphs = []
 	for datapoint in tqdm(train_set, ncols=80):
 		goal_num, world_num, tools, g, t = datapoint
@@ -117,7 +132,7 @@ def form_initial_dataset():
 		for action in actionSeq:
 			complete, new_graph, err = approx.execAction(goal_num, action, e); 
 			if err == '': 
-				df = df.append({'goal_num':goal_num, 'st':old_graph, 'at':action,'st+1':new_graph, 'r':1}, ignore_index=True)
+				df = df.append({'goal_num':goal_num, 'st':old_graph, 'at':action, 'p':1, 'st+1':new_graph, 'r':1}, ignore_index=True)
 				old_graph = new_graph
 	pickle.dump((df, init_graphs, test_set), open(filename, "wb"))
 	return data, df, init_graphs, test_set
@@ -125,20 +140,20 @@ def form_initial_dataset():
 def run_new_plan(model, init_graphs, all_actions):
 	g, goal_num, world_num = init_graphs[np.random.choice(range(len(init_graphs)))]
 	approx.initPolicy(domain, goal_num, world_num)
-	old_graphs, actions, new_graphs, i = [], [], [], 0
+	old_graphs, actions, p, new_graphs, i = [], [], [], [], 0
 	while True:
 		possible_actions = []
 		for action in all_actions: 
 			if approx.checkActionPossible(goal_num, action, e): possible_actions.append(action)
 		probs = model.policy(g, goal2vec[goal_num], goalObjects2vec[goal_num], possible_actions)
-		a = np.random.choice(possible_actions, p=probs.detach().numpy())
+		a = np.random.choice(possible_actions, p=probs.detach().numpy()); p.append(probs[possible_actions.index(a)])
 		complete, new_g, err = approx.execAction(goal_num, action, e);
 		old_graphs.append(g); actions.append(a); new_graphs.append(new_g)
 		g = new_g; i += 1;
 		if complete: r = [1]*len(old_graphs); break
 		elif i >= 40: r = [-1]*len(old_graphs); break
 		if err != '': r = [-1]*len(old_graphs); break
-	return pd.DataFrame({'goal_num':goal_num, 'st':old_graphs, 'at':actions, 'st+1':new_graphs, 'r':r}), r[0]
+	return pd.DataFrame({'goal_num':[goal_num]*len(old_graphs), 'st':old_graphs, 'at':actions, 'p':p, 'st+1':new_graphs, 'r':r}), r[0]
 
 def updateBuffer(model, init_graphs, all_actions, replay_buffer, num_runs):
 	dataframes = []; rewards = []
@@ -147,37 +162,36 @@ def updateBuffer(model, init_graphs, all_actions, replay_buffer, num_runs):
 		dataframes.append(plan_df); rewards.append(plan_r)
 	replay_buffer = pd.concat([replay_buffer]+dataframes, ignore_index=True)
 	if replay_buffer.shape[0] > memory_size: replay_buffer = replay_buffer[-1*memory_size:]
-	return replay_buffer, sum(rewards)/len(rewards)
+	return replay_buffer, avg(rewards)
 
 def get_training_data(replay_buffer, crowdsource_df, sample_size):
-	total_data = pd.concat([replay_buffer, crowdsource_df])
+	total_data = pd.concat([replay_buffer, crowdsource_df], ignore_index=True)
 	positive_data = total_data[total_data.r == 1].sample(sample_size)
-	# negative_data = total_data[total_data.r == -1].sample(sample_size)
-	return positive_data #pd.concat([positive_data, negative_data])
+	negative_data = total_data[total_data.r == -1].sample(sample_size)
+	return pd.concat([positive_data, negative_data], ignore_index=True)
 
 if __name__ == '__main__':
 	all_actions = get_all_possible_actions()
 	data, crowdsource_df, init_graphs, test_set = form_initial_dataset()
-	replay_buffer = pd.DataFrame(columns=['goal_num', 'st', 'at', 'st+1', 'r'])
+	replay_buffer = load_buffer()
 	model = A2C(data.features, data.num_objects, 2 * GRAPH_HIDDEN, 4, 3, etypes, torch.tanh)
 	optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9, weight_decay=0.0001)
 	l = nn.MSELoss()
 
 	while True:
 		# replay_buffer, avg_r = updateBuffer(model, init_graphs, all_actions, replay_buffer, 1)
-		for b in range(10):
+		# save_buffer(replay_buffer)
+		# print("Average reward =", avg_r)
+		for _ in range(10):
+			val_loss, total_loss, p_loss = [], [], []
 			dataset = get_training_data(replay_buffer, crowdsource_df, 10)
 			for ind in dataset.index:
-				goal_num, g, a, newg, r = dataset['goal_num'][ind], dataset['st'][ind], dataset['at'][ind], dataset['st+1'][ind], dataset['r'][ind]
+				goal_num, g, p, r = dataset['goal_num'][ind], dataset['st'][ind], dataset['p'][ind], dataset['r'][ind]
 				pred_val = model.value(g, goal2vec[goal_num], goalObjects2vec[goal_num])
-				# pred_val_new = model.value(newg, goal2vec[goal_num], goalObjects2vec[goal_num])
-				# prob = model.policy(g, goal2vec[goal_num], goalObjects2vec[goal_num])
-				# policy_loss = torch.mean(torch.sum((r + pred_val_new - pred_val) * -torch.log(torch.add(a, 0.0001)), 1))
-				value_loss = l(Variable(torch.Tensor(r)), pred_val)
-				#loss = torch.add(policy_loss, value_loss)
-				loss = value_loss
-				optimizer.zero_grad()
-				loss.backward()
-				optimizer.step()
-				print("Loss =", loss.data, " Value Loss =", value_loss.data)
-
+				p_loss.append((r - pred_val) * -torch.log(torch.tensor([p], dtype=torch.float)))
+				val_loss.append(F.smooth_l1_loss(torch.Tensor([r]), pred_val))
+			loss = torch.stack(p_loss).sum() + torch.stack(val_loss).sum()
+			optimizer.zero_grad()
+			loss.backward()
+			optimizer.step()
+			print("Loss =", loss.item(), " Value Loss =", avg(val_loss).item(), " Policy Loss =", avg(p_loss).item())
