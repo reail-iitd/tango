18ab89ed61fa782fc1a2eebdbc11d492cec7234c
shreshthtuli <shreshthtuli@gmail.com>
2020-06-01 15:29:59 +0530

    Revert "Update dataset_utils.py"
    This reverts commit 87db2304e86ead8181e5e6c1564c97b836353b80.

--- src/GNN/dataset_utils.py
+++ src/GNN/dataset_utils.py
@@ -104,7 +104,6 @@ def getDGLSequence(pathToDatapoint, globalNode, ignoreNoTool, e):
 	actionSeq = []; graphSeq = []
 	for action in datapoint.symbolicActions:
 		if not (str(action[0]) == 'E' or str(action[0]) == 'U'): actionSeq.append(action[0])
-		elif domain == 'factory': return None
 	for i in range(len(datapoint.metrics)):
 		if datapoint.actions[i] == 'Start' and AUGMENTATION == 1: graphSeq.append(convertToDGLGraph(datapoint.getGraph(i, embeddings=e)["graph_"+str(i)], globalNode, goal_num, getGlobalID(datapoint) if globalNode else -1))
 		elif datapoint.actions[i] == 'Start' and AUGMENTATION > 1: 
